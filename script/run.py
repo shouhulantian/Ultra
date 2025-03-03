a81b9ea2b083e0fcea1ad8e41b8bcb6f76430af0
shouhulantian <237448659@qq.com>
2025-03-03 18:49:10 +0100

    compact dataset v2

--- script/run.py
+++ script/run.py
@@ -165,7 +165,7 @@ def train_and_validate_time(cfg, model, train_data, valid_data, device, logger, 
                 batch = tasks.negative_sampling(train_data, batch, cfg.task.num_negative,
                                                 strict=cfg.task.strict_negative)
                 pred = parallel_model(train_data, batch)
-                os.system('nvidia-smi')
+                #os.system('nvidia-smi')
                 target = torch.zeros_like(pred)
                 target[:, 0] = 1
                 loss = F.binary_cross_entropy_with_logits(pred, target, reduction="none")
